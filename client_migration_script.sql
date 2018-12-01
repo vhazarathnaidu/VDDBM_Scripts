bbdc363486a92624a0af8113172b772b49edf253
Vuedata Technologies Pvt. Ltd <36773480+vuedata-technologies@users.noreply.github.com>
2018-12-02 01:32:04 +0530

    Update client_migration_script.sql

--- client_migration_script.sql
+++ client_migration_script.sql
@@ -1,5 +1,5 @@
 SQL_UP = """
-CREATE TABLE `sample_table` (
+CREATE TABLE `demo_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `author` varchar(40) NOT NULL,
@@ -9,5 +9,5 @@ CREATE TABLE `sample_table` (
 """
 
 SQL_DOWN = """
-DROP TABLE sample_table;
+DROP TABLE demo_table;
 """
