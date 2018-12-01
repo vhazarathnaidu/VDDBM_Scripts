f2d91b7d170d7ac532aa93dfcb89606400f63f2e
Vuedata Technologies Pvt. Ltd <36773480+vuedata-technologies@users.noreply.github.com>
2018-12-02 04:18:29 +0530

    Update client_migration_script.sql

--- client_migration_script.sql
+++ client_migration_script.sql
@@ -1,5 +1,5 @@
 SQL_UP = """
-CREATE TABLE `demo_table` (
+CREATE TABLE `demo_table_work` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `author` varchar(40) NOT NULL,
